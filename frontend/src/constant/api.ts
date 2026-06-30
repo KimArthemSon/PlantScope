fdf62aac4f61a8640cd63cabbf71e7862fcee469
kimson <sonkimarthem@gmail.com>
2026-07-01 03:28:28 +0800

    Fix api

--- frontend/src/constant/api.ts
+++ frontend/src/constant/api.ts
@@ -1,2 +1,4 @@
-export const api = import.meta.env.DNS_FIRST || "http://127.0.0.1:8000/"
-export const api_second = import.meta.env.DNS_SECOND || "http://127.0.0.1:8000"
+export const api = import.meta.env.VITE_DNS_FIRST || "http://127.0.0.1:8000/"
+export const api_second = import.meta.env.VITE_DNS_SECOND || "http://127.0.0.1:8000"
+
+console.log(api, "asdasdY Piu")
